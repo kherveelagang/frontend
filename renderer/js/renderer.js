14a72b74c12ce365f9eb507363afee3a07d0eee5
ite18 <iteeighteen@gmai.com>
2023-05-14 23:13:57 +0800

    Added Toastify alerts

--- renderer/js/renderer.js
+++ renderer/js/renderer.js
@@ -6,9 +6,31 @@ if (form) {
 
     const formData = new FormData(form);
 
+    let sentence = formData.get("sentence");
+    if (sentence.length <= 8) {
+      alertMessage("error", "Please input atleast 8 characters!");
+
+      return;
+    }
+
     const response = await window.axios.openAI(formData.get("sentence"));
     document.getElementById("sentence_formed").innerHTML = JSON.stringify(
       response.choices[0].text
     ).replace(/\\n/g, "");
   };
+}
+
+function alertMessage(status, sentence) {
+  window.Toastify.showToast({
+    text: sentence,
+    duration: 3000,
+    stopOnFocus: true,
+    style: {
+      textAlign: "center",
+      background: status == "error" ? "red" : "green",
+      color: "white",
+      padding: "5px",
+      marginTop: "2px",
+    },
+  });
 }
